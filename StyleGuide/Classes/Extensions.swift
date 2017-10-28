b65eaf7b1a142f31718e318ad9e3fdd11fb9863e
Shubham Agrawal <shubham.agr001@gmail.com>
2017-10-28 18:08:36 +0530

    Remove useless code

--- StyleGuide/Classes/Extensions.swift
+++ StyleGuide/Classes/Extensions.swift
@@ -40,37 +40,3 @@ extension UIView {
         }
     }
 }
-
-extension UIColor {
-    /**
-     Returns UIColor from hex color string of type #RRGGBB or #AARRGGBB
-     */
-    convenience init(hexString: String) {
-        let hexString2 = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-        let scanner = Scanner(string: hexString2)
-
-        if (hexString.hasPrefix("#")) {
-            scanner.scanLocation = 1
-        }
-
-        var color: UInt32 = 0
-        scanner.scanHexInt32(&color)
-
-        let mask: UInt32 = 0x000000FF
-        let r = (color >> 16) & mask
-        let g = (color >> 8) & mask
-        let b = (color) & mask
-
-        var a: UInt32 = 255
-        if hexString.characters.count > 7 {
-            a = (color >> 24) & mask
-        }
-
-        let red   = CGFloat(r) / 255.0
-        let green = CGFloat(g) / 255.0
-        let blue  = CGFloat(b) / 255.0
-        let alpha = CGFloat(a) / 255.0
-
-        self.init(red:red, green:green, blue:blue, alpha: alpha)
-    }
-}
